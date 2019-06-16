b5f5c5b4c1ffb3ef67c0ea39f4e8ba655ec36e36
Daniel Alves <danielalveslima36>
2019-06-16 15:43:49 -0300

    Implementação das funçoes salvar e buscar do Dao Redis

---
+++ ExercicioRedis/src/main/java/Model/Dao/Implementacao/DaoRedisImpl.java
@@ -0,0 +1,36 @@
+package Model.Dao.Implementacao;
+
+import Model.Dao.Interfaces.DaoBanco;
+import Model.Publicacao;
+import com.google.gson.Gson;
+import redis.clients.jedis.Jedis;
+
+public class DaoRedisImpl implements DaoBanco {
+
+    @Override
+    public Publicacao buscar(String id) {
+        Jedis jedis = new Jedis();
+
+        String publicacaoJson = jedis.get(id);
+
+        if (publicacaoJson != null){
+            Gson gson = new Gson();
+
+            Publicacao publicacao = gson.fromJson(publicacaoJson, Publicacao.class);
+            return publicacao;
+        }
+        return null;
+    }
+
+    @Override
+    public void salvar(Publicacao publicacao) {
+
+        Jedis jedis = new Jedis();
+        Gson gson = new Gson();
+
+        String publicacaoJson = gson.toJson(publicacao);
+
+        jedis.set(publicacao.getId(), publicacaoJson);
+
+    }
+}
